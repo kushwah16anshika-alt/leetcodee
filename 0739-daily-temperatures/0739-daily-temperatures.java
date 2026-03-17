711e7b36bf5455d33d71ade4fb65e1835ac72115
kushwah16anshika-alt <kushwah16anshika@gmail.com>
2026-03-18 02:42:12 +0530

    Time: 75 ms (27.6%), Space: 106.7 MB (67.16%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temp)
+    {
+
+        Stack<Integer>st=new Stack<>();
+        int n=temp.length;
+        int[]result=new int[n];
+        for(int i=n-1;i>=0;i--)
+        {
+        while(!st.isEmpty() && temp[st.peek()]<=temp[i])
+        {
+            st.pop();
+        }
+        if(st.isEmpty())
+        {
+           result[i]=0;
+        }
+        else
+        {
+           result[i]=st.peek()-i;
+        }
+          st.push(i);
+        }
+       return result;
+    }
+}
