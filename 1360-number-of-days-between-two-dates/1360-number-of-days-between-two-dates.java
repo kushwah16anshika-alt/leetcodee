17d87635dfc075087d6f8a1b8e4ce0968fe61023
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-06-11 15:41:57 +0530

    Time: 1 ms (98.59%), Space: 43.1 MB (40.4%) - LeetHub

--- 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.java
+++ 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.java
@@ -1,12 +1,36 @@
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+class Solution 
+{
+    public int daysBetweenDates(String date1, String date2) 
+    {
+        return Math.abs(countdays(date1) - countdays(date2));
 
-class Solution {
-    public int daysBetweenDates(String date1, String date2) {
+    }
+    public int countdays(String date)
+    {
+        String []parts=date.split("-");
+         int year=Integer.parseInt(parts[0]);
+         int month=Integer.parseInt(parts[1]);
+         int day=Integer.parseInt(parts[2]);
 
-        LocalDate d1 = LocalDate.parse(date1);
-        LocalDate d2 = LocalDate.parse(date2);
+         int [] daysinmonth={31,28,31,30,31,30,31,31,30,31,30,31};
 
-        return (int)Math.abs(ChronoUnit.DAYS.between(d1, d2));
+         int totalDays=(year-1)*365;
+
+         totalDays+=(year -1)/4;
+         totalDays-=(year-1)/100;
+         totalDays+=(year-1)/400;
+
+         for(int i=0;i<month-1;i++)
+         {
+            totalDays+=daysinmonth[i];
+         }
+         if(month>2 && ((year%4==0 && year%100 !=0 ||year%400==0)))
+         {
+            totalDays++;
+         }
+         totalDays+=day;
+
+       return totalDays;
+
     }
 }
