73836c8457c93c55cdcb5038132971701707a820
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-06-20 11:48:53 +0530

    Time: 6 ms (4.63%), Space: 48.3 MB (33%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -7,24 +7,22 @@ class Solution
 
         for(int i=0;i<height.length;i++)
         {
-           while(!st.isEmpty()&&height[i]>height[st.peek()]) 
-           {
-            int bottom=st.pop();
-
-            if(st.isEmpty())
+            while(!st.isEmpty()&&height[i]>height[st.peek()])
             {
-                break;
+                int bottom =st.pop();
+
+                if(st.isEmpty()) break;
+
+                int left= st.peek();
+
+                int width=i-left-1;
+
+                int hbound=Math.min(height[left],height[i])-height[bottom];
+
+                trappingwater+=width*hbound;
             }
-            int left=st.peek();
-
-            int width=i-left-1;
-
-            int heightbound=Math.min(height[i],height[left])-height[bottom];
-            trappingwater+=width*heightbound;
-
-           }
-           st.push(i);
-        } 
+            st.push(i);
+        }
         return trappingwater;
     }
 }
