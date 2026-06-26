f036b20e28e28e431f138913cc4eddd58cbb05e9
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-06-26 17:29:45 +0530

    Time: 0 ms (100%), Space: 46.5 MB (71.11%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution 
+{
+    public Node copyRandomList(Node head) 
+    {
+        if(head==null )
+        {
+            return null;
+        }
+        Node current=head;
+         HashMap<Node, Node> map = new HashMap<>();
+        while(current!=null)
+        {
+           Node copy = new Node(current.val);
+           map.put(current,copy);
+           current=current.next;
+        }
+        current =head;
+        while(current!=null)
+        {
+            Node copy=map.get(current);
+            copy.next=map.get(current.next);
+            copy.random=map.get(current.random);
+
+            current=current.next;
+        }
+
+        return map.get(head);
+    }
+}
