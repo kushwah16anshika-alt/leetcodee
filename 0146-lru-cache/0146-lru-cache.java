9b20327d74c488772e650e7d11ab50491709669a
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-06-29 23:01:45 +0530

    Time: 973 ms (5%), Space: 131.9 MB (35.42%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,56 +1,46 @@
-class LRUCache {
-    ArrayList<int[]>cache;
+class LRUCache 
+{
+    LinkedList<Integer> dll;
+    HashMap<Integer, Integer> cache;
     int capacity;
-
     public LRUCache(int capacity) 
     {
         this.capacity=capacity;
-        cache = new ArrayList<>();
+        cache=new HashMap<>();
+        dll=new LinkedList<>();
     }
-    
+    public void makeMRU(int key)
+    {
+        dll.remove(Integer.valueOf(key));
+        dll.addFirst(key);
+    }
     public int get(int key) 
     {
-      for(int i=0;i<cache.size();i++)
-      {
-        if(cache.get(i)[0]==key)
+        if(!cache.containsKey(key))
         {
-            int value=cache.get(i)[1];
-
-            int []temp=cache.get(i);
-            cache.remove(i);
-            cache.add(temp);
-
-
-            return value;
+            return -1;
         }
-      }
-      return -1;
+        makeMRU(key);
+        return cache.get(key);
     }
     
     public void put(int key, int value) 
     {
-        
-      for(int i=0;i<cache.size();i++)
-      {
-        if(cache.get(i)[0]==key)
+        if (cache.containsKey(key))
         {
-            cache.remove(i);
-            cache.add(new int[]{key, value});
-           
-            return;
+            cache.put(key,value);
+            makeMRU(key);
+        }else
+        {
+            if(dll.size()==capacity)
+            {
+                int lru=dll.removeLast();
+                cache.remove(lru);
+            }
+            dll.addFirst(key);
+            cache.put(key,value);
         }
     }
-    if(cache.size()==capacity)
-    {
-        cache.remove(0);
-        cache.add(new int[]{key, value});
-    }
-   else
-     {
-     cache.add(new int[]{key, value});
-     }
-    }
-
 }
 
 /**
