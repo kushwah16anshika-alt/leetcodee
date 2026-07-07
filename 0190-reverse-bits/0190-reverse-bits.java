43f66ea7d556a5013e772836ccde9315138847f4
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-07-07 10:50:35 +0530

    Time: 1 ms (61.22%), Space: 42.3 MB (84.59%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -5,10 +5,10 @@ class Solution
         int result=0;
         for(int i=0; i<32;i++)
         {
-            result<<=1;
-
-            result |=(n & 1);
+            result <<=1;
+            result |= (n & 1);
             n>>>=1;
+            
         }
         return result;
     }
