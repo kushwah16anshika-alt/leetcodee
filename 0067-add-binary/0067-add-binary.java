feab20985731d617876666760779def322a526d5
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-07-07 09:51:02 +0530

    Time: 1 ms (99.92%), Space: 43.4 MB (83.9%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -6,25 +6,22 @@ class Solution
         int j=b.length()-1;
         int carry=0;
 
-        StringBuilder sb=new StringBuilder();
-
+       StringBuilder sb=new StringBuilder();
         while(i>=0 || j>=0 || carry!=0)
         {
             int sum=carry;
-
             if(i>=0)
             {
-                sum+=a.charAt(i)-'0';
-                i--;
+               sum+= a.charAt(i)-'0';
+               i--;
             }
-             if(j>=0)
+            if(j>=0)
             {
-                sum+=b.charAt(j)-'0';
-                j--;
+               sum+= b.charAt(j)-'0';
+               j--;
             }
-
-            sb.append(sum%2);
-            carry=sum/2;
+           sb.append(sum%2);
+           carry=sum/2;
         }
         return sb.reverse().toString();
     }
