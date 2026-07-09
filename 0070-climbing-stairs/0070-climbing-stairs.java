be61a801ce4a64c2e30b5684da6882aaf2e16a19
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-07-09 23:15:17 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,19 @@
 class Solution {
-    public int climbStairs(int n)
+    public int climbStairs(int n) 
     {
+         // Agar sirf 1 ya 2 stairs hain to direct answer return kar
         if(n<=2)return n;
-        int a=1,b=2;
+
+        int a=1;
+        int b=2;
+
         for(int i=3;i<=n;i++)
         {
             int temp=a+b;
             a=b;
             b=temp;
         }
+
         return b;
     }
 }
